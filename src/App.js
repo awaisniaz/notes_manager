eaec64077ea5a3936724b3b41851415806f93b9b
owais <owais.niaz@targetofs.com>
2021-01-16 18:13:48 +0500

    Some removal from project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import Splachscreen from './components/splachscreen'
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
-import Home from './components/MainScreen/index'
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path='/'><Splachscreen /></Route>
-          <Route path='/home'><Home></Home></Route>
-        </Switch>
       </div>
     </Router>
   );
